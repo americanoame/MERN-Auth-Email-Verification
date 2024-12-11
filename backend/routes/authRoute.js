81e0a865f2fbffc0f454b54d8499b2445a06714d
Andre Silva <andrenations@gmail.com>
2024-12-10 20:03:07 -0500

    Add authentication routes for signup, signin, and logout

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { signin, signup, logout } from "../controllers/authController.js";
+
+const router = express.Router()
+
+
+router.post("/signin", signin);
+router.post("/signup", signup);
+router.post("/logout", logout);
+
+
+
+export default router;
