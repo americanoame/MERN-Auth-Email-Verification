4f8ef53a0f677a838f81773f76fcb36971683e2e
Andre Silva <andrenations@gmail.com>
2024-12-11 21:17:10 -0500

    Add cookie-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from 'cors';
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js"
+import cookieParser from "cookie-parser";
 
 const port = process.env.PORT || 3000;
 
@@ -19,6 +20,11 @@ app.use(cors({
 // Body parser middleware to parse the body of the request (req.body)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+// Cookie parser middleware to parse the cookies from the request object
+// alows to acces reqest.cookies and since  the cookie is called JWT we can access it by req.cookies.JWT
+// we can do this in the authMiddleware.js file
+app.use(cookieParser());
 
 
 app.use("/api/auth", authRoute);
