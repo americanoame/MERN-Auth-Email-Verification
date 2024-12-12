5bed8fa34627892569235735fbdec9052a66d78c
Andre Silva <andrenations@gmail.com>
2024-12-11 21:25:45 -0500

    add routes

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,12 +1,19 @@
 import express from "express";
-import { signin, signup, logout } from "../controllers/authController.js";
+import { signin, signup, logout, verifyEmail, forgotPassword, resetPassword, checkAuth } from "../controllers/authController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router()
+
+router.get("/check-auth", verifyToken, checkAuth);
 
 
 router.post("/signin", signin);
 router.post("/signup", signup);
 router.post("/logout", logout);
+
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 
 
